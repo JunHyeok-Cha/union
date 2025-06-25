731791d01612c7fff5aa55362ec4489f7ae17a06
chaBee <jhcha990917@gmail.com>
2025-06-25 13:49:22 +0900

    first commit

---
+++ src/main/java/com/example/practice2nd/Practice2ndApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice2nd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice2ndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practice2ndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practice2nd/Practice2ndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice2nd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practice2ndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
